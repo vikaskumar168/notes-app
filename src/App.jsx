21c1a90c83008b553d5b296c2eb0bd72e3d07a35
vikas <vikaskmr2411@gmail.com>
2024-04-03 16:39:11 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from './Components/Header'
+import './App.css'
+import Notes from './Components/Notes'
+
+function App() {
+
+
+  return (
+    <>
+     <div className='main'>
+     <Header />
+     <Notes />
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/CreateNotes.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const CreateNotes = ({ inputText, setInputText, saveHandler }) => {
+
+    const char = 100;
+    const charLimit = char - inputText.length
+
+  return (
+    <div className="note">
+      <textarea
+        cols={10}
+        rows={5}
+        placeholder="Type..."
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+        maxLength={100}
+      ></textarea>
+      <div className="note_footer">
+        <span className="label">{charLimit} character Left</span>
+        <button className="note_save" onClick={saveHandler}>
+          Save
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CreateNotes;
